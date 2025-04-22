0a125d12ee62b6fd9ba96eb0a2f4839771d6322d
fbdmac <fajarletters@gmail.com>
2025-04-22 11:34:45 -0700

    hover and lock apps

--- src/constants/globalConstants.ts
+++ src/constants/globalConstants.ts
@@ -1,1 +1,3 @@
 export const CDN_ROOT_URL = "https://d1af2lvvh100pg.cloudfront.net/";
+export const URL_LOCK_ICON = CDN_ROOT_URL + "portfolio/lock_icon-32x32.png";
+export const URL_HEADSHOT = CDN_ROOT_URL + "shared/circle_headshot.png";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,46 @@
 import "./style.css";
-import { CDN_ROOT_URL } from "../../constants/globalConstants";
+import {
+  CDN_ROOT_URL,
+  URL_HEADSHOT,
+  URL_LOCK_ICON,
+} from "../../constants/globalConstants";
 import { APP_LIST } from "../../constants/appList";
+import { useState } from "preact/hooks";
 
 export function Home() {
+  const [appList, setAppList] = useState(APP_LIST);
+  const [selectedApp, setSelectedApp] = useState(null);
+
   const renderProjects = () => {
     return APP_LIST.map((app) => {
+      const projectBlockClassList = ["project-block", "noselect"];
+      if (selectedApp === app) {
+        projectBlockClassList.push("project-block--selected");
+      }
+      const projectBlockClass = projectBlockClassList.join(" ");
+
       return (
-        <div class="project-block">
-          <div class="project-block__img-container">
-            <img src={app.iconUrl} alt="" />
+        <div class="project-block__wrapper">
+          <div
+            class={projectBlockClass}
+            onClick={() => {
+              if (selectedApp == app) {
+                setSelectedApp(null);
+              } else {
+                setSelectedApp(app);
+              }
+            }}
+          >
+            <div class="project-block__img-container">
+              <img src={app.iconUrl} alt="" />
+            </div>
+            <div class="project-block__name-container">
+              <h2>{app.name}</h2>
+              <p>{app.shortDescription}</p>
+            </div>
+            <p class="project-block__year">{app.year}</p>
           </div>
-          <div class="project-block__name-container">
-            <h2>{app.name}</h2>
-            <p>{app.shortDescription}</p>
-          </div>
-          <p class="project-block__year">{app.year}</p>
+          <img src={URL_LOCK_ICON} alt="" class="project-block__lock-icon" />
         </div>
       );
     });
@@ -23,10 +49,13 @@ export function Home() {
   return (
     <div class="home">
       <div class="header"></div>
-      <div class="project-list">{renderProjects()}</div>
+      <div class="project-list">
+        <p class="project-list__info">Click to lock</p>
+        {renderProjects()}
+      </div>
       <div class="hero">
         <div class="hero__image-container">
-          <img src={CDN_ROOT_URL + "shared/circle_headshot.png"} alt="" />
+          <img src={URL_HEADSHOT} alt="" />
         </div>
         <div class="hero__text">
           <h1>
