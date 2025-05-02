b02116547eddc38a358fd66cdf965a32ded22390
fbdmac <fajarletters@gmail.com>
2025-05-02 23:30:17 +0700

    prod functions

--- src/helpers/backendController.ts
+++ src/helpers/backendController.ts
@@ -4,8 +4,10 @@ export async function portfolioChat(
   newMessage: string,
   prevMessages: { role: string; content: string }[]
 ): Promise<string> {
-  const BASE_URL = "http://127.0.0.1:5001/fbd-pages/us-central1/";
-  const ENDPOINT = BASE_URL + "portfolioChat";
+  // FOR EMULATORS
+  //   const ENDPOINT = "http://127.0.0.1:5001/fbd-pages/us-central1/portfolioChat/";
+
+  const ENDPOINT = "https://portfoliochat-jbbpuvm5sq-uc.a.run.app ";
 
   const res = await axios.post(ENDPOINT, {
     newMessage,
